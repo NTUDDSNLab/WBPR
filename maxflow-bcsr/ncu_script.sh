046846148457a3d8737fe5676fb457d599661463
PunchSHadow <littleuniverse24@gmail.com>
2024-11-24 15:50:46 +0800

    Update ncu script

--- maxflow-bcsr/ncu_script.sh
+++ maxflow-bcsr/ncu_script.sh
@@ -14,43 +14,43 @@ snap_file_path="/home/sylab/PunchShadow/WBPR/snap_cap1_data"
 
 echo "BCSR--------------------"
 
-# # Command 1 - 
-# echo "1: Running movielens-t-i_super.txt..."
-# echo "\t start TC: $(date)"
-# ncu $ncu_flags -o $profiling_file_path/movielen-u-i_bcsr_TC.ncu-rep ./maxflow -v 2 -f $bipartite_file_path/movielens-t-i_super.txt -a 0 -t 1 -a 0
-# echo "\t end TC: $(date)"
-# echo "\t start TLPNS: $(date)"
-# ncu $ncu_flags -o $profiling_file_path/movielen-u-i_bcsr_TLPNS.ncu-rep ./maxflow -v 2 -f $bipartite_file_path/movielens-t-i_super.txt -a 0 -t 1 -a 1
-# echo "\t end TLPNS: $(date)"
-# echo "-------------------------------------------"
+# Command 1 - 
+echo "1: Running dbpedia-location_super.txt..."
+echo "\t start TC: $(date)"
+ncu $ncu_flags -o $profiling_file_path/dbpedia-location_super_bcsr_TC.ncu-rep ./maxflow -v 2 -f $bipartite_file_path/dbpedia-location_super.txt -a 0 -t 1 -a 0
+echo "\t end TC: $(date)"
+echo "\t start TLPNS: $(date)"
+ncu $ncu_flags -o $profiling_file_path/dbpedia-location_super_bcsr_TLPNS.ncu-rep ./maxflow -v 2 -f $bipartite_file_path/dbpedia-location_super.txt -a 0 -t 1 -a 1
+echo "\t end TLPNS: $(date)"
+echo "-------------------------------------------"
 
 # # Command 2 -
 # echo "2: Running youtube_super.txt..."
 # echo "\t start TC: $(date)"
-# ncu $ncu_flags -o $profiling_file_path/youtube_bcsr_TC.ncu-rep ./maxflow -v 2 -f $bipartite_file_path/youtube_super.txt -a 0 -t 1 -a 0
+# ncu $ncu_flags -o $profiling_file_path/youtube_rcsr_TC.ncu-rep ./maxflow -v 2 -f $bipartite_file_path/youtube_super.txt -a 0 -t 1 -a 0
 # echo "\t end TC: $(date)"
 # echo "\t start TLPNS: $(date)"
-# ncu $ncu_flags -o $profiling_file_path/youtube_bcsr_TLPNS.ncu-rep ./maxflow -v 2 -f $bipartite_file_path/youtube_super.txt -a 0 -t 1 -a 1
+# ncu $ncu_flags -o $profiling_file_path/youtube_rcsr_TLPNS.ncu-rep ./maxflow -v 2 -f $bipartite_file_path/youtube_super.txt -a 0 -t 1 -a 1
 # echo "\t end TLPNS: $(date)"
 # echo "-------------------------------------------"
 
 # Command 3 -
-echo "3: Running roadNet_PA_cap1.txt..."
+echo "3: Running soc-LiveJournal1_cap1.txt..."
 echo "\t start TC: $(date)"
-ncu $ncu_flags -o $profiling_file_path/roadNet_PA_bcsr_TC.ncu-rep ./maxflow -v 2 -f $snap_file_path/roadNet_PA_cap1.txt -a 0 -t 1 -a 0
+ncu $ncu_flags -o $profiling_file_path/soc-LiveJournal1_cap1_bcsr_TC.ncu-rep ./maxflow -v 2 -f $snap_file_path/soc-LiveJournal1_cap1.txt -a 0 -t 1 -a 0
 echo "\t end TC: $(date)"
 echo "\t start TLPNS: $(date)"
-ncu $ncu_flags -o $profiling_file_path/roadNet_PA_bcsr_TLPNS.ncu-rep ./maxflow -v 2 -f $snap_file_path/roadNet_PA_cap1.txt -a 0 -t 1 -a 1
+ncu $ncu_flags -o $profiling_file_path/soc-LiveJournal1_cap1_bcsr_TLPNS.ncu-rep ./maxflow -v 2 -f $snap_file_path/soc-LiveJournal1_cap1.txt -a 0 -t 1 -a 1
 echo "\t end TLPNS: $(date)"
 echo "-------------------------------------------"
 
-# Command 4 -
-echo "4: Running cit-Patents_cap1.txt..."
-echo "\t start TC: $(date)"
-ncu $ncu_flags -o $profiling_file_path/cit-Patents_bcsr_TC.ncu-rep ./maxflow -v 2 -f $snap_file_path/cit-Patents_cap1.txt -a 0 -t 1 -a 0
-echo "\t end TC: $(date)"
-echo "\t start TLPNS: $(date)"
-ncu $ncu_flags -o $profiling_file_path/cit-Patents_bcsr_TLPNS.ncu-rep ./maxflow -v 2 -f $snap_file_path/cit-Patents_cap1.txt -a 0 -t 1 -a 1
-echo "\t end TLPNS: $(date)"
+# # Command 4 -
+# echo "4: Running cit-Patents_cap1.txt..."
+# echo "\t start TC: $(date)"
+# ncu $ncu_flags -o $profiling_file_path/cit-Patents_rcsr_TC.ncu-rep ./maxflow -v 2 -f $snap_file_path/cit-Patents_cap1.txt -a 0 -t 1 -a 0
+# echo "\t end TC: $(date)"
+# echo "\t start TLPNS: $(date)"
+# ncu $ncu_flags -o $profiling_file_path/cit-Patents_rcsr_TLPNS.ncu-rep ./maxflow -v 2 -f $snap_file_path/cit-Patents_cap1.txt -a 0 -t 1 -a 1
+# echo "\t end TLPNS: $(date)"
 
 
